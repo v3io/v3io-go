c93f1b64c0fc7da36646fca6f6d03c046cce1403
imakhlin <igorm@iguazio.com>
2019-09-18 21:13:48 +0300

    support both octal and decimal file mode

--- pkg/dataplane/types.go
+++ pkg/dataplane/types.go
@@ -22,6 +22,7 @@ import (
 	"encoding/xml"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -98,7 +99,7 @@ type CommonPrefix struct {
 	Mode         FileMode `xml:"Mode"`         // octal number, e.g. 040775
 	GID          string   `xml:"GID"`          // Hexadecimal representation of GID (e.g. "3e8" -> i.e. "0x3e8" == 1000)
 	UID          string   `xml:"UID"`          // Hexadecimal representation of UID (e.g. "3e8" -> i.e. "0x3e8" == 1000)
-	InodeNumber  *uint32  `xml:"InodeNumber"`  // iNode number
+	InodeNumber  *uint64  `xml:"InodeNumber"`  // iNode number
 }
 
 type FileMode string
@@ -115,13 +116,24 @@ func mode(v3ioFileMode FileMode) os.FileMode {
 	const S_IFMT = 0xf000
 	const IP_OFFMASK = 0x1fff
 
-	// Convert 16 bit octal representation of V3IO into decimal 32 bit representation of Go
-	mode, err := strconv.ParseUint(string(v3ioFileMode), 8, 32)
-	if err != nil {
-		panic(err)
+	// Note, File mode from different API's has different base.
+	// For example Scan API returns file mode as decimal number (base 10) while ListDir as Octal (base 8)
+	var sFileMode = string(v3ioFileMode)
+	if strings.HasPrefix(sFileMode, "0") {
+		// Convert 16 bit octal representation of V3IO into decimal 64 bit representation of Go
+		if mode, err := strconv.ParseUint(sFileMode, 8, 64); err != nil {
+			panic(err)
+		} else {
+			golangFileMode := ((mode & S_IFMT) << 17) | (mode & IP_OFFMASK)
+			return os.FileMode(golangFileMode)
+		}
+	} else {
+		if mode, err := strconv.ParseUint(sFileMode, 10, 64); err != nil {
+			panic(err)
+		} else {
+			return os.FileMode(mode)
+		}
 	}
-	golangFileMode := ((mode & S_IFMT) << 17) | (mode & IP_OFFMASK)
-	return os.FileMode(golangFileMode)
 }
 
 type GetContainerContentsOutput struct {
