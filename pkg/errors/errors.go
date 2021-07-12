f745bf2779f4094c38b8ba6cd5b0d00c1206094d
Felix <felixgel@gmail.com>
2021-07-12 19:04:21 +0300

    Include response in err struct

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -2,6 +2,9 @@ package v3ioerrors
 
 import (
 	"errors"
+
+	v3io "github.com/v3io/v3io-go/pkg/dataplane"
+	"github.com/valyala/fasthttp"
 )
 
 var ErrInvalidTypeConversion = errors.New("Invalid type conversion")
@@ -12,6 +15,11 @@ var ErrTimeout = errors.New("Timed out")
 type ErrorWithStatusCode struct {
 	error
 	statusCode int
+}
+
+type ErrorWithStatusCodeAndResponse struct {
+	ErrorWithStatusCode
+	response *v3io.Response
 }
 
 func NewErrorWithStatusCode(err error, statusCode int) ErrorWithStatusCode {
@@ -27,4 +35,33 @@ func (e ErrorWithStatusCode) StatusCode() int {
 
 func (e ErrorWithStatusCode) Error() string {
 	return e.error.Error()
+}
+
+func NewErrorWithStatusCodeAndResponse(err error,
+	statusCode int,
+	response *v3io.Response) ErrorWithStatusCodeAndResponse {
+
+	// Copy response struct since original response will be released if error is encountered
+	var _copiedResponse *v3io.Response
+	if response != nil {
+		copiedHTTPResponse := &fasthttp.Response{}
+		if response.HTTPResponse != nil {
+			response.HTTPResponse.CopyTo(copiedHTTPResponse)
+		}
+		_copiedResponse = &v3io.Response{
+			Output:       response.Output,
+			Context:      response.Context,
+			Error:        response.Error,
+			HTTPResponse: copiedHTTPResponse,
+		}
+	}
+
+	return ErrorWithStatusCodeAndResponse{
+		ErrorWithStatusCode: NewErrorWithStatusCode(err, statusCode),
+		response:            _copiedResponse,
+	}
+}
+
+func (e ErrorWithStatusCodeAndResponse) Response() *v3io.Response {
+	return e.response
 }
