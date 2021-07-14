1e1b1866325f2ba101e372e9b2b226b517e5a146
Felix <felixgel@gmail.com>
2021-07-14 17:51:39 +0300

    Revert "Added IncludeResponseInError flag"
    This reverts commit 2f5fc0cfa62d5eef442a43b5ae4199583c04591d.

--- pkg/errors/errors.go
+++ pkg/errors/errors.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	v3io "github.com/v3io/v3io-go/pkg/dataplane"
+	"github.com/valyala/fasthttp"
 )
 
 var ErrInvalidTypeConversion = errors.New("Invalid type conversion")
@@ -40,9 +41,24 @@ func NewErrorWithStatusCodeAndResponse(err error,
 	statusCode int,
 	response *v3io.Response) ErrorWithStatusCodeAndResponse {
 
+	// Copy response struct since original response will be released if error is encountered
+	var _copiedResponse *v3io.Response
+	if response != nil {
+		copiedHTTPResponse := &fasthttp.Response{}
+		if response.HTTPResponse != nil {
+			response.HTTPResponse.CopyTo(copiedHTTPResponse)
+		}
+		_copiedResponse = &v3io.Response{
+			Output:       response.Output,
+			Context:      response.Context,
+			Error:        response.Error,
+			HTTPResponse: copiedHTTPResponse,
+		}
+	}
+
 	return ErrorWithStatusCodeAndResponse{
 		ErrorWithStatusCode: NewErrorWithStatusCode(err, statusCode),
-		response:            response,
+		response:            _copiedResponse,
 	}
 }
 
