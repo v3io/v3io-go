3e1e48d98de86ae9499b9c4604baece0e265cbb9
omesser <odedmesser@gmail.com>
2022-01-23 01:17:35 +0200

    Added access key generation script

---
+++ hack/script/generate_access_key.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# requires httpie, jq
+echo "Creating control session"
+echo '{"data":{"type":"session","attributes":{"plane": "control", "username": "'${V3IO_CONTROLPLANE_USERNAME}'","password": "'${V3IO_CONTROLPLANE_PASSWORD}'"}}}' | http --verify=no ${V3IO_CONTROLPLANE_URL}/api/sessions --session igz_user
+
+echo "Creating Data Access Key"
+ACCESS_KEY_ID=$( echo '{"data":{"type":"access_key","attributes":{"plane":"data","label":"v3io-go-test"}}}' | http --verify=no ${V3IO_CONTROLPLANE_URL}/api/self/get_or_create_access_key --session igz_user | jq -r ".data.id")
+
+echo "V3IO_DATAPLANE_ACCESS_KEY=${ACCESS_KEY_ID}"
