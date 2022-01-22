4d23281a30e66286ad9fd1fef2a20e0efaf6d7f2
omesser <odedmesser@gmail.com>
2022-01-23 01:22:44 +0200

    verify envs

--- hack/script/generate_access_key.sh
+++ hack/script/generate_access_key.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# verify required env
+: "${V3IO_CONTROLPLANE_URL:?Need to set V3IO_CONTROLPLANE_URL to non-empty value}"
+: "${V3IO_CONTROLPLANE_USERNAME:?Need to set V3IO_CONTROLPLANE_USERNAME to non-empty value}"
+: "${V3IO_CONTROLPLANE_PASSWORD:?Need to set V3IO_CONTROLPLANE_PASSWORD to non-empty value}"
+
 # requires httpie, jq
 echo "Creating control session"
 echo '{"data":{"type":"session","attributes":{"plane": "control", "username": "'${V3IO_CONTROLPLANE_USERNAME}'","password": "'${V3IO_CONTROLPLANE_PASSWORD}'"}}}' | http --verify=no ${V3IO_CONTROLPLANE_URL}/api/sessions --session igz_user
