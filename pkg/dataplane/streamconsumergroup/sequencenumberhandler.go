f7231114c4b30ed2d9f9431f4545050a010784c1
alxtkr77 <3098237+alxtkr77@users.noreply.github.com>
2024-08-28 10:21:45 +0300

    Revert "[NUC-233] Troubeshooting" (#153)

--- pkg/dataplane/streamconsumergroup/sequencenumberhandler.go
+++ pkg/dataplane/streamconsumergroup/sequencenumberhandler.go
@@ -72,10 +72,6 @@ func (snh *sequenceNumberHandler) stop() error {
 }
 
 func (snh *sequenceNumberHandler) markShardSequenceNumber(shardID int, sequenceNumber uint64) error {
-	err := snh.member.streamConsumerGroup.checkShardExists(shardID, sequenceNumber)
-	if err != nil {
-		return errors.Wrapf(err, "Failed checking shard exists. Current sequenceNumber %v", snh.markedShardSequenceNumbers[shardID])
-	}
 	snh.markedShardSequenceNumbers[shardID] = sequenceNumber
 
 	return nil
